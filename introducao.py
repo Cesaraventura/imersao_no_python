d6d440a4a6a34f519f40953490874110359059a9
César Augusto Ventura <cesar.ventura.lima@gmail.com>
2022-01-24 10:08:21 -0300

    exercício dia 2

--- introducao.py
+++ introducao.py
@@ -1,2 +1,10 @@
 # iniciando projeto
 print("iniciando projeto")
+# exercício 02
+nome = input("Digite seu nome: ")
+print('é um prazer te conhecer,', nome)
+# exercício 03
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite mais um número:'))
+s = n1 + n2
+print('a soma vale:',s)
